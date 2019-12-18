efebdc4f93a75dff7027829c2c4c35247fc844d4
Jasmin Herkelmann <Jasmin.Herkelmann@avantisystems.eu>
2019-12-18 10:18:28 +0100

    no message

--- Completed/Minutes/Minutes/Entry.swift
+++ Completed/Minutes/Minutes/Entry.swift
@@ -3,7 +3,7 @@ import Foundation
 class Entry: Codable
 {
     var title:   String = "Hello"
-    var content: String = "There"
+    var content: String = "123"
     
     var id: String = UUID().uuidString
     
